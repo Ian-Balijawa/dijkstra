248d1870179be85975431472278d62c9d9648adb
ian balijawa <ianbalijawa16@gmail.com>
2023-04-24 12:43:00 +0300

    add vite support

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import { PathfindingVisualizer } from "./components/Visualizer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer />
+    </div>
+  );
+}
